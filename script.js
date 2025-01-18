79cd4c91e944e7a4be074c47aae3bee34a51443c
Karthees <kartheeswaran68@gmail.com>
2025-01-18 18:46:05 +0530

    Add Projects

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function showMenu() {
 function hideMenu() {
   menu.style.display = "none";
 }
+
+function openPop(event) {
+  event.target.parentElement.children[3].style.display="block";
+}
+
+function closePop(event) {
+  event.target.parentElement.style.display="none";
+}
